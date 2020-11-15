39a01317a1abf9769e1a4ba48773fa90b8f49d88
yehudit96 <yehudit.reyzer@gmail.com>
2020-11-15 23:27:54 +0200

    update classification code

--- classifiers/chirps_evaluation.py
+++ classifiers/chirps_evaluation.py
@@ -6,13 +6,16 @@ import sys
 rank_path = '../rules_ranking'
 sys.path.append(rank_path)
 from rank_utils import AP
+from baseline_classifier import score_GloVe
 
 random.seed(1)
+
+skip = 15
 
 def main():
     with open('datasets/Kian/eval_an', 'rb') as f:
         eval_rules = pickle.load(f)
-
+    
     eval_rules = random.sample(eval_rules, 500)
     rules, x, y = zip(*eval_rules)
     with open('best_rf', 'rb') as f_clf:
@@ -30,8 +33,9 @@ def main():
         print("number of positive rules between {} to {}".format(bin_s, bin_e))
         print(sum(y[bin_s:bin_e]))
     print('classifier AP: {}'.format(AP(y)))
-    
     print_top_10(y, rules_model)
+    
+    
     sort_chirps = sorted(proba, key=lambda a:a[0][4], reverse=True)
     _, y, rules_chirps, _, pred = zip(*sort_chirps)
     print('sort by chirps rank')
@@ -41,10 +45,20 @@ def main():
     print('chirps AP: {}'.format(AP(y)))
     print_top_10(y, rules_chirps)
     
+    sort_GloVe = sorted(proba, key=lambda a:score_GloVe(a), reverse=True)
+    _, y, rules_GloVe, _, pred = zip(*sort_GloVe)
+    print('sort by GloVe rank')
+    for bin_s, bin_e in bins:
+        print("number of positive rules between {} to {}".format(bin_s, bin_e))
+        print(sum(y[bin_s:bin_e]))
+    print('GloVe AP: {}'.format(AP(y)))
+    print_top_10(y, rules_GloVe)
+    
     
 def print_top_10(labels, rules):
-    for label, rule in list(zip(labels, rules))[0::2][:10]:
-        print('{}: {}'.format(label, rule))
+    print('skip: {}'.format(skip))
+    for i, label, rule in list(zip(range(1, len(labels)+1), labels, rules))[0::skip][:10]:
+        print('{}: {}'.format(label, rule, i))
 
 
 if __name__ == "__main__":
